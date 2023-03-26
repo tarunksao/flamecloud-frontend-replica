cf7bdc16e460c650c7603f0eff4fe5e619aa1eee
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-03-26 18:02:23 +0530

    Corrected CSS

--- src/Pages/LandingPageComponents/Welcome.jsx
+++ src/Pages/LandingPageComponents/Welcome.jsx
@@ -30,9 +30,12 @@ export default function Welcome() {
         <Box m='auto'>
             <Text style={headingStyle} fontSize={{base:'7px',xl:'16px'}} lineHeight={{base:'8.5px',xl:'19px'}} textAlign={'center'}>WELCOME TO CREATIVE PROXIES</Text>
             <Heading style={headingStyle2} fontSize={{base:'28px',xl:'56px'}} lineHeight={{base:'30px',xl:'67px'}} width={{base:'315px',xl:'824px'}}>We are the <Highlight query='Fastest' styles={{color:'#077BFF'}}>Fastest</Highlight> and the <Highlight query='Most' styles={{color:'#16D113'}}>Most</Highlight> <Highlight query='Reliable' styles={{color:'#16D113'}}>Reliable</Highlight> Proxy Service since 2020</Heading>
-            <Image src={'blueStar.svg'} pos={'absolute'} top={{base:'95px', xl:'193px'}} left={{base:'266px', xl:'800px'}} w={{base:'12px', xl:''}} />
-            <Image src={'greenStar.svg'} pos={'absolute'} top={{base:'124px', xl:'267px'}} left={{base:'88px', xl:'525px'}} w={{base:'11.5px', xl:''}} transform={'matrix(-1, 0, 0, 1, 0, 0)'} />
+            
+            <Image src={'blueStar.svg'} pos={'absolute'} top={{base:'95px', xl:'193px'}} left={{base:'266px', xl:'800px'}} w={{base:'12px', xl:'30px'}} />
+            <Image src={'greenStar.svg'} pos={'absolute'} top={{base:'124px', xl:'267px'}} left={{base:'88px', xl:'525px'}} w={{base:'11.5px', xl:'30px'}} transform={{base:'matrix(-1, 0, 0, 1, 0, 0)', xl:'matrix(1, 0, 0, 1, 0, 0)'}} />
+            
             <Text style={bodyStyle} fontSize={{base:'10px',xl:'22px'}} lineHeight={{base:'12px',xl:'26px'}} width={{base:'226.5px',xl:'509px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus augue sit amet mollis molestie.</Text>
+            
             <Link to='/proxyLocation'>
                 <Button 
                     w={{base:'157px',xl:'223px'}} 
