bb2ec616779c418bdec31a69c83db10c89ad2c2d
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-03-26 16:09:25 +0530

    Making Landing page Responsive

--- src/Pages/LandingPageComponents/Resell.jsx
+++ src/Pages/LandingPageComponents/Resell.jsx
@@ -3,16 +3,12 @@ import crown from '../../Images/crown.svg';
 import { CheckIcon } from "@chakra-ui/icons";
 
 const headingStyle = {
-    fontSize:'20px',
     fontWeight:'600',
-    lineHeight:'24px',
     letterSpacing:'-0.02em',
     color:'#111822'
 }
 
 const subHeadingStyle = {
-    fontSize:'16px',
-    lineHeight:'19px',
     letterSpacing:'-0.02em',
     opacity:0.5,
     color:'#111822'
@@ -23,40 +19,41 @@ export default function Resell() {
         <>
             <Box
                 m='auto'
-                mb='180px'
-                w={{base:'206px', xl:'708px'}} 
+                mb={{base:'70.5px', xl:'180px'}}
+                w={{base:'315px', xl:'708px'}} 
                 boxSizing= 'border-box' 
                 background= 'rgba(30, 30, 30, 0.01)'
                 border= '1px solid rgba(30, 30, 30, 0.1)'
                 borderRadius= '5px'
             >
-                <HStack p='21.5px 28px' gap='34px' alignItems={'flex-start'}>
-                    <VStack alignItems={'flex-start'} gap='8px'>
+                <HStack justifyContent={'start'} p={{base:'8px 12px', xl:'21.5px 28px'}} gap={{base:'17px', xl:'34px'}} alignItems={'flex-start'}>
+                    <VStack alignItems={'flex-start'} justifyContent={'start'} gap={{base:'0px', xl:'8px'}}>
                         <Box
-                            w='40px'
-                            h='40px'
+                            w={{base:'21.4px', xl:'40px'}} 
+                            h={{base:'21.4px', xl:'40px'}}
                             background= 'linear-gradient(135deg, #FFDE2E 0%, #E38B08 100%)'
                             borderRadius={'8px'}
                             display={'flex'}
                             justifyContent={'center'}
                             alignItems={'center'}
+                            p={{base:'4.3px', xl:'8px'}} 
                         >
-                            <Image src={crown} alt='crown.svg' w='24px' h='24px' />
+                            <Image src={crown} alt='crown.svg' w={{base:'12.85px', xl:'24px'}} h={{base:'12.85px', xl:'24px'}} />
                         </Box>
-                        <Text style={headingStyle}>Become a Reseller</Text>
-                        <Text style={subHeadingStyle}>200+ Happy Resellers</Text>
+                        <Text style={headingStyle} fontSize={{base:'10px', xl:'20px'}} lineHeight={{base:'12px', xl:'24px'}}>Become a Reseller</Text>
+                        <Text style={subHeadingStyle} fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}>200+ Happy Resellers</Text>
                     </VStack>
-                    <VStack alignItems={'flex-start'}>
-                        <Text style={subHeadingStyle}>Plan Includes:</Text>
-                        <HStack alignItems={'flex-start'} gap='53px'>
+                    <VStack alignItems={'flex-start'} justifyContent={'start'}>
+                        <Text style={subHeadingStyle} fontSize={{base:'7px', xl:'16px'}}>Plan Includes:</Text>
+                        <HStack alignItems={'flex-start'} justifyContent={'start'} gap={{base:'20px', xl:'53px'}}>
                             <VStack alignItems={'flex-start'} justifyContent={'start'}>
-                                <Text><CheckIcon color='#077BFF' mr='8px' />Whitelabel IP Pool</Text>
-                                <Text><CheckIcon color='#077BFF' mr='8px' />Whitelabel Subnets</Text>
-                                <Text><CheckIcon color='#077BFF' mr='8px' />Residential Proxy</Text>
+                                <Text fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}><CheckIcon color='#077BFF' mr='8px' />Whitelabel IP Pool</Text>
+                                <Text fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}><CheckIcon color='#077BFF' mr='8px' />Whitelabel Subnets</Text>
+                                <Text fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}><CheckIcon color='#077BFF' mr='8px' />Residential Proxy</Text>
                             </VStack>
                             <VStack alignItems={'flex-start'} justifyContent={'start'}>
-                                <Text><CheckIcon color='#077BFF' mr='8px' />Custom Bandwidth</Text>
-                                <Text><CheckIcon color='#077BFF' mr='8px' />Proxy Support</Text>
+                                <Text fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}><CheckIcon color='#077BFF' mr='8px' />Custom Bandwidth</Text>
+                                <Text fontSize={{base:'7px', xl:'16px'}} lineHeight={{base:'10px', xl:'19px'}}><CheckIcon color='#077BFF' mr='8px' />Proxy Support</Text>
                             </VStack>
                         </HStack>
                     </VStack>
